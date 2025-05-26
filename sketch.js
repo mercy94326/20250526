402c47aac67c76b77c2a1b4b24a60c35849dd9a6
mercy94326 <mercy94326@gmail.com>
2025-05-26 10:07:55 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ function setup() {
 }
 
 function modelReady() {
-  // 模型載入完成，可選擇顯示訊息
+  console.log("FaceMesh 模型已載入");
 }
 
 function draw() {
@@ -27,11 +27,16 @@ function draw() {
   if (predictions.length > 0) {
     const keypoints = predictions[0].scaledMesh;
 
+    // 列印所有點位資訊
+    console.log(keypoints);
+
     // 在第94點畫紅色圓，大小為50x50
     const [x, y] = keypoints[94];
     noFill();
     stroke(255, 0, 0);
     strokeWeight(4);
-    ellipse(x, y, 50, 50); // 修改圓圈大小為50x50
+    ellipse(x, y, 50, 50);
+  } else {
+    console.log("沒有預測結果");
   }
 }
