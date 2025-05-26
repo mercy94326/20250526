26f9b28c7ae5c80c045b2a1149b6493feb0c4b25
mercy94326 <mercy94326@gmail.com>
2025-05-26 10:12:12 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -22,20 +22,25 @@ function modelReady() {
 }
 
 function draw() {
+  // 翻轉影像
+  push();
+  translate(width, 0);
+  scale(-1, 1);
   image(video, 0, 0, width, height);
+  pop();
 
   if (predictions.length > 0) {
     const keypoints = predictions[0].scaledMesh;
 
-    // 列印所有點位資訊
+    // 列印所有點位資訊（用於確認鼻尖位置）
     console.log(keypoints);
 
-    // 在第94點畫紅色圓，大小為50x50
-    const [x, y] = keypoints[94];
+    // 在鼻尖（第168點）畫紅色圓，大小為50x50
+    const [x, y] = keypoints[168];
     noFill();
     stroke(255, 0, 0);
     strokeWeight(4);
-    ellipse(x, y, 50, 50);
+    ellipse(width - x, y, 50, 50); // 修正翻轉後的座標
   } else {
     console.log("沒有預測結果");
   }
